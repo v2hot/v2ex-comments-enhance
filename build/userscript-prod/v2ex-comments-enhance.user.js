1e086df82a270615ca2ac2a99d53ceedce083268
pipecraft <pipecraft.net@gmail.com>
2023-05-16 09:46:24 +0800

    First commit of v2ex-comments-enhance

---
+++ build/userscript-prod/v2ex-comments-enhance.user.js
@@ -0,0 +1,21 @@
+// ==UserScript==
+// @name                 V2EX Comments Enhance
+// @name:zh-CN           V2EX 评论增强
+// @namespace            https://github.com/v2hot/v2ex-comments-enhance
+// @homepage             https://github.com/v2hot/v2ex-comments-enhance#readme
+// @supportURL           https://github.com/v2hot/v2ex-comments-enhance/issues
+// @version              0.0.1
+// @description          try to take over the world!
+// @description:zh-CN    让世界更美好！
+// @icon                 https://www.v2ex.com/favicon.ico
+// @author               Pipecraft
+// @license              MIT
+// @match                https://*/*
+// @match                http://*/*
+// ==/UserScript==
+//
+;(() => {
+  "use strict"
+  {
+  }
+})()
