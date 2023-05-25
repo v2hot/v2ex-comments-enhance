460c604168045a4a59f149cf24cc96e122f25866
pipecraft <pipecraft.net@gmail.com>
2023-05-25 16:08:48 +0800

    显示热门回复

---
+++ src/utils/index.ts
@@ -0,0 +1,13 @@
+import { $ } from "browser-extension-utils"
+
+export const getFloorNumberElement = (replyElement: HTMLElement) =>
+  $("span.no", replyElement)
+
+export const getFloorNumber = (replyElement: HTMLElement) => {
+  const numberElement = getFloorNumberElement(replyElement)
+  if (numberElement) {
+    return Number.parseInt(numberElement.textContent || "", 10) || undefined
+  }
+
+  return undefined
+}
