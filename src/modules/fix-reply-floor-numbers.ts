c440dee41baa49ecb7c828e01f3b5815bf1da316
pipecraft <pipecraft.net@gmail.com>
2023-05-27 00:08:56 +0800

    Fix scrollbar style, fix fix-reply-floor-numbers

--- src/modules/fix-reply-floor-numbers.ts
+++ src/modules/fix-reply-floor-numbers.ts
@@ -1,6 +1,6 @@
-import { $, $$ } from "browser-extension-utils"
+import { $ } from "browser-extension-utils"
 
-import { getFloorNumberElement } from "../utils"
+import { getFloorNumberElement, getReplyElements } from "../utils"
 
 const getTopicReplies = async (topicId: string) => {
   const url = `${location.protocol}//${location.host}/api/replies/show.json?topic_id=${topicId}`
@@ -16,39 +16,34 @@ const updateReplyElements = (
   replies: Array<Record<string, unknown>>,
   page = 1
 ) => {
-  const replyElements = $$('.cell[id^="r_"]')
-  let index = 0
-  let elementOffset = 0
-  const offset = (page - 1) * 100
+  let floorNumberOffset = 0
+  const dataOffSet = (page - 1) * 100
   const length = Math.min(replies.length - (page - 1) * 100, 100)
   for (let i = 0; i < length; i++) {
-    const reply = replies[i + offset]
+    const reply = replies[i + dataOffSet]
     const id = reply.id
     const element = $("#r_" + id)
     if (!element) {
       console.info(
-        `[V2EX 回复增强] 屏蔽或隐藏的回复: #${
-          index + offset + elementOffset + 1
-        }, 用户 ID: ${reply.member?.username}, 回复 ID: ${
-          reply.id
-        }, 回复内容: ${reply.content}`
+        `[V2EX.REP] 屏蔽或隐藏的回复: #${i + dataOffSet + 1}, 用户 ID: ${
+          reply.member?.username
+        }, 回复 ID: ${reply.id}, 回复内容: ${reply.content}`
       )
-      elementOffset++
+      floorNumberOffset++
       continue
     }
 
     element.found = true
-    if (elementOffset > 0) {
+    if (floorNumberOffset > 0) {
       const numberElement = getFloorNumberElement(element)
       if (numberElement) {
-        numberElement.textContent = String(index + offset + elementOffset + 1)
+        numberElement.textContent = String(i + dataOffSet + 1)
       }
     }
-
-    index++
   }
 
-  if (elementOffset > 0) {
+  if (floorNumberOffset > 0) {
+    const replyElements = getReplyElements()
     const v2exPolishModel = $(".v2p-model-mask")
     // 如果 API 返回的数据不是最新，实际页面的回复数会比 API 里的多。更新多的部分
     for (const element of replyElements) {
@@ -65,7 +60,7 @@ const updateReplyElements = (
       if (numberElement) {
         const oldNumber = Number.parseInt(numberElement.textContent || "", 10)
         if (oldNumber) {
-          numberElement.textContent = String(oldNumber + elementOffset)
+          numberElement.textContent = String(oldNumber + floorNumberOffset)
         }
       }
     }
