6b7a4f1d77e0086aee2347041ff1fac5f8fda917
pipecraft <pipecraft.net@gmail.com>
2023-06-02 01:53:15 +0800

    给用户头像加上链接

---
+++ src/modules/add-link-to-avatars.ts
@@ -0,0 +1,17 @@
+import { $, createElement, getAttribute } from "browser-extension-utils"
+
+export const addLinkToAvatars = (replyElement: HTMLElement) => {
+  const memberLink = $('a[href^="/member/"]', replyElement)
+  if (memberLink && memberLink.firstChild?.tagName === "IMG") {
+    return
+  }
+
+  const avatar = $("img.avatar", replyElement)
+  if (memberLink && avatar) {
+    const memberLink2 = createElement("a", {
+      href: getAttribute(memberLink, "href"),
+    })
+    avatar.after(memberLink2)
+    memberLink2.append(avatar)
+  }
+}
