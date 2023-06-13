64ee0707b39339e82817eaaca64da8fd9d7d999b
pipecraft <pipecraft.net@gmail.com>
2023-06-13 08:32:49 +0800

    自动签到功能

---
+++ src/modules/daily-check-in.ts
@@ -0,0 +1,64 @@
+import { getValue, setValue } from "browser-extension-storage"
+import { $ } from "browser-extension-utils"
+
+import { getOnce, sleep } from "../utils"
+import { updateOnce } from "./update-once"
+
+const storageKey = "dailyCheckIn"
+let retryCount = 0
+
+const fetchCheckInApi = async (once: string) => {
+  const url = `${location.protocol}//${location.host}/mission/daily/redeem?once=${once}`
+
+  try {
+    const response = await fetch(url)
+
+    if (response.status === 200) {
+      return await response.text()
+    }
+  } catch (error) {
+    console.error(error)
+    retryCount++
+    if (retryCount < 3) {
+      await sleep(1000)
+      return fetchCheckInApi(once) as Promise<string>
+    }
+  }
+}
+
+export const dailyCheckIn = async () => {
+  const once = getOnce()
+  if (!once) {
+    return
+  }
+
+  const lastCheckInDate = (await getValue(storageKey)) as number
+  if (lastCheckInDate) {
+    const now = Date.now()
+    if (
+      now - lastCheckInDate < 86_400_000 &&
+      new Date(now).getUTCDate() === new Date(lastCheckInDate).getUTCDate()
+    ) {
+      // 已签到
+      return
+    }
+  }
+
+  const result = (await fetchCheckInApi(once)) as string
+  if (result.includes("每日登录奖励已领取")) {
+    console.info("[V2EX.REP] 签到成功")
+    await setValue(storageKey, Date.now())
+    const checkInLink = $(`a[href^="/mission/daily"]`)
+    if (checkInLink) {
+      const box = checkInLink.closest(".box")
+      if (box) {
+        box.nextElementSibling?.remove()
+        box.remove()
+      }
+    }
+  } else {
+    console.error("[V2EX.REP] 签到失败")
+  }
+
+  await updateOnce()
+}
---
+++ src/modules/update-once.ts
@@ -0,0 +1,30 @@
+import { $, $$, getAttribute, setAttribute } from "browser-extension-utils"
+
+const fetchOnce = async () => {
+  const url = `${location.protocol}//${location.host}/poll_once`
+  const response = await fetch(url)
+
+  try {
+    if (response.status === 200) {
+      return await response.text()
+    }
+  } catch (error) {
+    console.error("[V2EX.REP] Unable to refresh once", error)
+  }
+}
+
+export const updateOnce = async () => {
+  const once = await fetchOnce()
+  if (once) {
+    window.once = once
+    if ($("#once")) {
+      $("#once").value = once
+    }
+
+    const links = $$(`a[href*="once="]`)
+    for (const link of links) {
+      const href = getAttribute(link, "href")
+      setAttribute(link, "href", href.replace(/once=\d+/, `once=${once}`))
+    }
+  }
+}
